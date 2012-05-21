18ac66702f8e3bf8c2bd4e97caf5ab96eb0ac820
Peter Klipfel <peter@klipfel.me>
2012-05-20 19:35:30 -0600

    title tests; changed prev tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,21 +2,35 @@ require 'spec_helper'
 
 describe "StaticPages" do
   describe "Home Page" do
-  	it "should have content 'Sample App'" do
+  	it "should have h1 'Sandbox App'" do
   		visit "/static_pages/home"
-  		page.should have_content("Sample App")
+  		page.should have_selector('h1', :text => "Sandbox App")
   	end
-  end  	
+  	it "should have title 'Home'" do
+  		visit "/static_pages/home"
+  		page.should have_selector('title', :text => "Sandbox App")
+    end
+  end
+
   describe "Help Page" do
-  	it "should have content 'help'" do
+  	it "should have h1 'help'" do
   		visit "/static_pages/help"
-  		page.should have_content("Help")
+  		page.should have_selector("h1", :text => "Help")
+    end
+    it "should have title 'Help'" do
+      visit "/static_pages/help"
+      page.should have_selector("title", :text => "Help")
   	end
   end
+  
   describe "About Page" do
-  	it "should have content 'About'" do
+  	it "should have h1 'About'" do
   		visit "/static_pages/about"
-  		page.should have_content("about")
+  		page.should have_selector("h1", :text => "About")
+    end
+    it "should have title 'About'" do
+      visit "/static_pages/about"
+      page.should have_selector("title", :text => "About")
   	end
   end
 end
