b766ca3158332aa16a356c33ab7be1ecbbdc5f4a
Peter Klipfel <peter@klipfel.me>
2012-05-20 19:41:58 -0600

    Home page passes tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,7 +8,7 @@ describe "StaticPages" do
   	end
   	it "should have title 'Home'" do
   		visit "/static_pages/home"
-  		page.should have_selector('title', :text => "Sandbox App")
+  		page.should have_selector('title', :text => "Home")
     end
   end
 
