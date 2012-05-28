79c3fbe2c4c564cab7c17a44f8bd9d9ce6eacf12
Peter Klipfel <peter@klipfel.me>
2012-05-27 23:03:22 -0600

    changed routing

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,44 +3,44 @@ require 'spec_helper'
 describe "StaticPages" do
   describe "Home Page" do
   	it "should have h1 'Sandbox'" do
-  		visit "/static_pages/home"
+  		visit root_path
   		page.should have_selector('h1', :text => "Sandbox")
   	end
   	it "should have title 'Home'" do
-  		visit "/static_pages/home"
+  		visit root_path
   		page.should have_selector('title', :text => "Home")
     end
   end
 
   describe "Help Page" do
   	it "should have h1 'help'" do
-  		visit "/static_pages/help"
+  		visit help_path
   		page.should have_selector("h1", :text => "Help")
     end
     it "should have title 'Help'" do
-      visit "/static_pages/help"
+      visit help_path
       page.should have_selector("title", :text => "Help")
   	end
   end
   
   describe "About Page" do
   	it "should have h1 'About'" do
-  		visit "/static_pages/about"
+  		visit about_path
   		page.should have_selector("h1", :text => "About")
     end
     it "should have title 'About'" do
-      visit "/static_pages/about"
+      visit about_path
       page.should have_selector("title", :text => "About")
   	end
   end
 
   describe "Contact Page" do
     it "should have h1 'Contact'" do
-      visit "/static_pages/contact"
+      visit contact_path
       page.should have_selector("h1", :text => "Contact")
     end
     it "should have title 'Contact'" do
-      visit "/static_pages/contact"
+      visit contact_path
       page.should have_selector("title", :text => "Contact")
     end
   end
