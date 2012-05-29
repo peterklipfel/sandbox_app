8fe3d3cb32f06ade356ee87382a20842edf44da0
Peter Klipfel <peter@klipfel.me>
2012-05-28 18:56:00 -0600

    refactored tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,47 +1,25 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+  subject{ page }
   describe "Home Page" do
-  	it "should have h1 'Sandbox'" do
-  		visit root_path
-  		page.should have_selector('h1', :text => "Sandbox")
-  	end
-  	it "should have title 'Home'" do
-  		visit root_path
-  		page.should have_selector('title', :text => "Home")
-    end
+    before{ visit root_path }
+    check_h1_title("Sandbox", full_title(''))
+  	it { should_not have_selector('title', :text => "Home")}
   end
 
   describe "Help Page" do
-  	it "should have h1 'help'" do
-  		visit help_path
-  		page.should have_selector("h1", :text => "Help")
-    end
-    it "should have title 'Help'" do
-      visit help_path
-      page.should have_selector("title", :text => "Help")
-  	end
+    before{ visit help_path }
+    check_h1_title('Help', full_title('Help'))
   end
   
   describe "About Page" do
-  	it "should have h1 'About'" do
-  		visit about_path
-  		page.should have_selector("h1", :text => "About")
-    end
-    it "should have title 'About'" do
-      visit about_path
-      page.should have_selector("title", :text => "About")
-  	end
+    before{ visit about_path }
+    check_h1_title('About', full_title('About'))
   end
 
   describe "Contact Page" do
-    it "should have h1 'Contact'" do
-      visit contact_path
-      page.should have_selector("h1", :text => "Contact")
-    end
-    it "should have title 'Contact'" do
-      visit contact_path
-      page.should have_selector("title", :text => "Contact")
-    end
+    before{ visit contact_path }
+    check_h1_title('Contact', full_title('Contact'))
   end
 end
