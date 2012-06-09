af641f470ad70faa90e251e6a6e3605f856f711e
Peter Klipfel <peter@klipfel.me>
2012-06-08 19:33:04 -0600

    user 'show' page passes tests

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -5,10 +5,12 @@ describe "UserPages" do
 	describe  "signup page" do
 		before { visit signup_path }
 		check_h1_title('Sign Up', full_title('Sign Up'))
-	end
-	describe  "ProfielPage" do
-		let(:user) { FactoryGirl.create(:user) }
-		before{ visit user_path(user) }
-		check_h1_title(user.name, user.name)
+	end	
+  describe "profile page" do
+	  let(:user) { FactoryGirl.create(:user) }
+	  before { visit user_path(user) }
+	  # can't use 'check_h1_title(user.name, user.name)' for some reason
+	  it { should have_selector('h1',    :text => user.name) }
+	  it { should have_selector('title', :text => user.name) }
 	end
 end
