f6cb0556d67d3646e21e5c5a42183fcb62b0b873
Peter Klipfel <peter@klipfel.me>
2012-06-30 17:23:54 -0600

    added signin page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,19 @@
 class SessionsController < ApplicationController
+	def new
+	
+	end
+
+	def create
+		@user = User.find_by_email(params[:session][:email])
+		if @user && @user.authenticate(params[:session][:password])
+			#sign in 
+		else
+			flash.now[:error] = "Couldn't find that user/email combination" 
+			render 'new'
+		end
+	end
+
+	def destroy
+	
+	end
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -4,7 +4,35 @@ describe "AuthenticationPages" do
 	subject { page }
   describe "sign in page" do
   	before { visit signin_path }
-  	it { should have_selector('h1',    :text => 'Sign In') }
-    it { should have_selector('title', :text => 'Sign In') }
   end
+
+  describe 'signin' do
+		before { visit signin_path }
+		describe 'with invalid information' do
+			before { click_button 'Sign In' }
+	  	it { should have_selector('h1',    :text => 'Sign In') }
+	    it { should have_selector('title', :text => 'Sign In') }
+
+			describe 'flash message shouldn\'nt persist' do
+				before { click_link 'Home'}
+				it { should_not have_selector('div.alert.alert-error') }
+			end
+		end
+
+		describe 'with valid information' do
+			let(:user) { FactoryGirl.create(:user) }
+			before do
+				fill_in :email,					:with => user.email
+				fill_in :password,			:with => user.password
+				click_button 'Sign In'
+			
+			end
+
+			it { should have_selector('title',  :text => user.name) }
+			it { should have_link('Profile', 		:href => user_path(user)) }
+			it { should have_link('Sign Out', 	:href => signout_path) }
+			it { should_not have_link('Sign in',:href => signin_path) }
+		end
+
+	end
 end
