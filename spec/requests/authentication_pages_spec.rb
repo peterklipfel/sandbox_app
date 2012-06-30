be6a10816be8b03a8dae1fa53bf4baa76808a4a3
Peter Klipfel <peter@klipfel.me>
2012-06-30 03:13:52 -0600

    sessions controller added

---
+++ spec/requests/authentication_pages_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe "AuthenticationPages" do
+	subject { page }
+  describe "sign in page" do
+  	before { visit signin_path }
+  	it { should have_selector('h1',    :text => 'Sign In') }
+    it { should have_selector('title', :text => 'Sign In') }
+  end
+end
