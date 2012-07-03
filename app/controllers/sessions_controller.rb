5b8fc154e0e93775499bbbab03d8091b8fc53364
Peter Klipfel <peter@klipfel.me>
2012-07-02 23:01:30 -0600

    login token added to user model

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,8 @@ class SessionsController < ApplicationController
 	def create
 		@user = User.find_by_email(params[:session][:email])
 		if @user && @user.authenticate(params[:session][:password])
-			#sign in 
+			sign_in user
+			redirect_to user
 		else
 			flash.now[:error] = "Couldn't find that user/email combination" 
 			render 'new'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120603040201) do
+ActiveRecord::Schema.define(:version => 20120703045811) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -19,8 +19,10 @@ ActiveRecord::Schema.define(:version => 20120603040201) do
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
     t.string   "password_digest"
+    t.string   "login_token"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["login_token"], :name => "index_users_on_login_token"
 
 end
