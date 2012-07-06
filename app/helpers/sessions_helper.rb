4a9214ee4dcd77ffae8d474939555e9817757cf2
Peter Klipfel <peter@klipfel.me>
2012-07-05 19:13:30 -0600

    added sign in functionality

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,20 @@
 module SessionsHelper
+
+	def sign_in(user)
+		cookies.permanent[:login_token] = user.login_token
+		self.current_user = user
+	end
+
+	def current_user=(user)
+		@current_user = user
+	end
+
+	def current_user
+		@current_user ||= User.find_by_login_token(cookies[:login_token])
+	end
+
+	def signed_in?
+		!current_user.nil?
+	end
+	
 end
