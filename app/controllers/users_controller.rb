24282bde2547f6ee9b528a0474df16b4c571a495
Peter Klipfel <peter@klipfel.me>
2012-07-08 17:17:18 -0600

    edit-user complete & authentication tests added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,17 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])    
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = 'Profile Updated'
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end   
   end
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -29,7 +29,7 @@ describe "AuthenticationPages" do
 			end
 			it { should have_selector('h1',  		:text => user.name) }
 			it { should have_link('Profile', 		:href => user_path(user)) }
-			it { should have_link('Settings', 	:href => edit_user_path(user) }
+			it { should have_link('Settings', 	:href => edit_user_path(user)) }
 			it { should have_link('Sign Out', 	:href => signout_path) }
 			it { should_not have_link('Sign in',:href => signin_path) }
 			it { should_not have_selector('div',:text => "Couldn't find that user/email combination")}
@@ -48,5 +48,40 @@ describe "AuthenticationPages" do
 		describe 'with invalid information' do
 			before { click_button 'Save Changes' }
 
-			it { should have_content 'Error' }
-end
+			it { should have_content 'error' }
+		end
+
+		describe 'with valid information' do
+			let(:new_email) { 'fwoop@shoop.oop' }
+			let(:new_name) { 'Anoo Idantt' }
+			before do
+				fill_in 'Name', with: new_name
+				fill_in 'Email', with: new_email
+				fill_in 'Password', with: user.password
+				fill_in 'Confirm Password', with: user.password
+				click_button 'Save Changes' 
+			end
+			it { should have_selector('title', :text => 'Sandbox App | ' + new_name) }
+			it { should have_selector('div.alert.alert-success') }
+			it { should have_link('Sign Out', :href => signout_path) }
+			specify { user.reload.name.should == new_name }
+			specify { user.reload.email.should == new_email }
+		end
+	end
+
+	describe 'authorization' do
+
+		describe 'for users that aren\'t signed in' do
+			describe 'in users controller'
+				describe 'visiting the edit page' do
+					before { visit edit_user_path(user) }
+					it { should have_selector('title', :text => 'Sign In') }
+				end
+				describe 'submitting to the update action' do
+					before { put user_path(user) }
+					specify { response.should redirect_to(signin_path) }
+				end
+			end
+		end
+	end
+end	
