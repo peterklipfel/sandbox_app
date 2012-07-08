dd9bd8c44c76acc13bcac675f13e4f4c3f0be784
Peter Klipfel <peter@klipfel.me>
2012-07-08 13:39:53 -0600

    all tests pass

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -22,13 +22,13 @@ describe "AuthenticationPages" do
 		describe 'with valid information' do
 			let(:user) { FactoryGirl.create(:user) }
 			before do
-				fill_in :email,					with: user.email
-				fill_in :password,			with: user.password
+				fill_in 'Email',					with: user.email
+				fill_in 'Password',				with: user.password
 				click_button 'Sign In'
 			
 			end
 			it { should have_selector('h1',  :text => user.name) }
-			it { should have_selector('div', :text => "Couldn't find that user/email combination")}
+			it { should_not have_selector('div', :text => "Couldn't find that user/email combination")}
 			it { should have_link('Profile', 		:href => user_path(user)) }
 			it { should have_link('Sign Out', 	:href => signout_path) }
 			it { should_not have_link('Sign in',:href => signin_path) }
