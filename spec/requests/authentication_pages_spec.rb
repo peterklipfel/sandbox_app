ba5759f8436c8874529e922b7eaddbdbce274b64
Peter Klipfel <peter@klipfel.me>
2012-07-08 14:42:58 -0600

    added 'edit user' section

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -27,11 +27,26 @@ describe "AuthenticationPages" do
 				click_button 'Sign In'
 			
 			end
-			it { should have_selector('h1',  :text => user.name) }
-			it { should_not have_selector('div', :text => "Couldn't find that user/email combination")}
+			it { should have_selector('h1',  		:text => user.name) }
 			it { should have_link('Profile', 		:href => user_path(user)) }
+			it { should have_link('Settings', 	:href => edit_user_path(user) }
 			it { should have_link('Sign Out', 	:href => signout_path) }
 			it { should_not have_link('Sign in',:href => signin_path) }
+			it { should_not have_selector('div',:text => "Couldn't find that user/email combination")}
 		end
 	end
+
+	describe 'Edit' do
+		let(:user) { FactoryGirl.create(:user) }
+		before { visit edit_user_path(user) }
+		describe 'Page' do
+			it { should have_selector('h1', :text => 'Update Your Profile') }
+			it { should have_selector('title', :text => 'Edit User') }
+			it { should have_link('change', :href => 'http://gravatar.com/emails') }
+		end
+
+		describe 'with invalid information' do
+			before { click_button 'Save Changes' }
+
+			it { should have_content 'Error' }
 end
--- spec/support/utitlities.rb
+++ spec/support/utitlities.rb
@@ -11,3 +11,13 @@ def check_h1_title(h1, title)
 	it { should have_selector('h1', :text => h1)}
   it { should have_selector('title', :text => title)}
 end
+
+def sign_in(user)
+	visit(signin_path)
+	fill_in :email, with: 	 user.email
+	fill_in :password, with: user.password
+	click_button 'Sign In'
+	# the following line is used in case the testing environment
+	# is not using capybara
+	cookies[:login_token] = user.login_token
+end
